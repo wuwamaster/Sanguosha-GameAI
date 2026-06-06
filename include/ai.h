108ce40a75a4fe1d4ebc11bf69b048f63fa39589
wuwamaster <983883075@qq.com>
2026-06-06 21:38:53 +0800

    4444

--- include/ai.h
+++ include/ai.h
@@ -6,3 +6,5 @@ int ai_should_use_tao_to_save(GameState* gs, int saver_idx, int dying_idx);
 const char* ai_get_last_psych_message();
 int ai_get_last_emotion();
 void ai_generate_psych_message(GameState* gs, int ai_idx, Action act);
+
+//iji
